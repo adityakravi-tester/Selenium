e7a4a5fc1fec5771eb83bdde44674d58136c1929
adity <adity@Aditya-Ravi>
2024-11-26 09:01:22 +0530

    added week2 day1 assignment

---
+++ MavenProject/src/main/java/week2/day1/LibraryManagement.java
@@ -0,0 +1,9 @@
+package week2.day1;
+
+public class LibraryManagement {
+	public static void main(String[] args) {
+		Library library = new Library();
+		library.addBook("The Prince");
+		library.issueBook();
+	}
+}
